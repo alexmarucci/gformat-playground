68ad32f6ae21f60fbe402aae1bef9aa2c23eb501
Alessio Marucci <alessio@phntms.com>
2020-03-07 17:30:18 +0000

    prod live

---
+++ precache-manifest.0f46ed76d46c1f1b4edd18a793400dcf.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6902dad247a2a2ef317fafa018e33d8",
+    "url": "/sw-events.js"
+  },
+  {
+    "revision": "6978a616c585d03cb5b542a891995efb",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "fa1f143e25945d2aa321",
+    "url": "/js/chunk-vendors.a43390b2.js"
+  },
+  {
+    "revision": "625308f81870a075419f",
+    "url": "/js/app.619725db.js"
+  },
+  {
+    "revision": "82614143a5183cc2b7ac2c08f40edd64",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7636e1061d96791514cd21bc17ef6af5",
+    "url": "/img/logo.7636e106.png"
+  },
+  {
+    "revision": "fa1f143e25945d2aa321",
+    "url": "/css/chunk-vendors.e91f9f97.css"
+  },
+  {
+    "revision": "625308f81870a075419f",
+    "url": "/css/app.aefef5c3.css"
+  },
+  {
+    "revision": "f610fb0e740eddf9e4c8c9e50a352dca",
+    "url": "/c7c2b96b72f45c7ac64b.worker.js"
+  }
+];
